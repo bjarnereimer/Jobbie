acaeb1ac962d35b8411004157457d22f71cfce8c
Bjarne Reimer Pedersen <brp@himmel-og-hav.org>
2024-01-01 17:20:42 +0100

    Added database

---
+++ data/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO PERSON (first_name, last_name) VALUES ('Hubert', 'Baller');
+INSERT INTO PERSON (first_name, last_name) VALUES ('Albert', 'Tucker');
+INSERT INTO PERSON (first_name, last_name) VALUES ('Fido', 'Bak');
